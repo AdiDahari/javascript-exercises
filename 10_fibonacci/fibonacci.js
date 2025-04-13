bf37f36955998e93c98a27894eb8487bb092d2d1
Adi Dahari <dahariadi16@gmail.com>
2025-04-13 17:54:22 +0300

    exercise 10_fibonacci finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let nums = [0, 1];
+  if (n < 0) return "OOPS";
+  if (n < 2) return nums[n];
 
+  for (let i = 2; i < n; i++) {
+    nums[i % 2] += nums[(i + 1) % 2];
+  }
+
+  return nums[0] + nums[1];
 };
 
 // Do not edit below this line
