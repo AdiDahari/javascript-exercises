7c3f117fc855b7a64db37823811ea4cd75a7ac0a
Adi Dahari <dahariadi16@gmail.com>
2025-04-13 18:16:13 +0300

    exercise 12_findTheOldest finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (peopleArr) {
+  return peopleArr.sort((p1, p2) => getAge(p2) - getAge(p1))[0];
 };
+
+function getAge(person) {
+  return (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
