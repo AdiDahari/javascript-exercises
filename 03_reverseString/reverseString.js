3d5029eb70c5a440b074d2bb044dac79570f0f20
Adi Dahari <dahariadi16@gmail.com>
2025-04-13 16:14:13 +0300

    one-liner implemented

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function (str) {
-  let res = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    res += str[i];
-  }
-
-  return res;
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
