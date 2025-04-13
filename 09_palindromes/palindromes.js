c842568bf57cb41a882d11a404262e496bcd7e98
Adi Dahari <dahariadi16@gmail.com>
2025-04-13 17:43:24 +0300

    exercise 09_palindromes finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter((x) => alphanumerical.includes(x))
+    .join("");
 
+  const len = cleanStr.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (cleanStr[i] !== cleanStr[len - i - 1]) return false;
+  }
+
+  return true;
 };
+
+palindromes("a!aba.a");
 
 // Do not edit below this line
 module.exports = palindromes;
