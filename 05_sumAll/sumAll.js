51f573f0eb421ee7d5ddb2385d78c013d19fcb68
Adi Dahari <dahariadi16@gmail.com>
2025-04-13 16:51:50 +0300

    exercise 05_sumAll finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (firstNum, lastNum) {
+  if (
+    !Number.isInteger(firstNum) ||
+    firstNum <= 0 ||
+    !Number.isInteger(lastNum) ||
+    lastNum <= 0
+  ) {
+    return "ERROR";
+  }
+  const maxNum = Math.max(lastNum, firstNum);
+  const minNum = Math.min(lastNum, firstNum);
+  let res = 0;
+  for (let i = minNum; i <= maxNum; i++) {
+    res += i;
+  }
 
+  return res;
 };
 
 // Do not edit below this line
