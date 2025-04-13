0b94abd5044b47b588b1165b8f7db639d9f628f8
Adi Dahari <dahariadi16@gmail.com>
2025-04-13 17:14:44 +0300

    exercise 07_tempConversion finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+  // x °C ≘ (x ×⁠ 9/5⁠ + 32) °F
+  return Math.floor((tempInF - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInC) {
+  // x °F ≘ (x − 32) × ⁠5/9 °C
+  return Math.floor((tempInC * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
